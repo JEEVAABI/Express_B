a0187a78b8bc43c6c2a4580e1626c0c4a6ca52e7
JEEVAABI <jeevaabishake@gmail.com>
2023-05-08 11:48:09 +0530

    Update. servicepage content updated

---
+++ servicePage.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const userRouter = express.Router()
+
+const listOfServices = [
+    {
+        
+        'service' : 'app development'
+    },
+    {
+        
+        'service' : 'web development'
+    },
+    {
+        
+        'service' : 'full stack'
+    },
+    {
+        
+        'service' : 'backend devlopment'
+    },
+    {
+        
+        'service' : 'front end development'
+    }
+]
+userRouter.get('/',(request,response)=>{
+    response.send('this is service page')
+})
+
+userRouter.get('/:id([0-9]{1})',(request,response)=>{
+    response.send(request.user)
+    
+})
+userRouter.param('id',(request,response,next,id)=>{
+    request.user = listOfServices[id-1]
+    next()
+
+})
+
+
+
+module.exports = userRouter
